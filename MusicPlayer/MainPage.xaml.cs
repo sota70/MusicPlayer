c4e086ef51288ab1f0fdb42d6ab761271917b1f3
sota70 <sota52350@gmail.com>
2021-02-22 12:17:51 +0900

    プロジェクト ファイルを追加します。

---
+++ MusicPlayer/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using MusicPlayer.MusicList;
+using MusicPlayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+namespace MusicPlayer
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page, IElementGetter
+    {
+        private static MainPage instance;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            instance                                  = this;
+            IMusic sampleMusic                        = new SampleMusic();
+            /*　もし曲名とアーティスト名を変えたい場合はこうする
+            ((IMusicNameSetter)sampleMusic).MusicName = "NameChanged";
+            ((IMusicArtistSetter)sampleMusic).Artist  = "ArtistChanged";
+            */
+            sampleMusic.Play();
+        }
+
+        public static MainPage getInstance()
+        {
+            return instance;
+        }
+
+        public TextBlock textBlock
+        {
+            get => NameDisplay;
+        }
+    }
+}
---
+++ MusicPlayer/MusicList/SampleMusic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using MusicPlayer.Interface;
+
+namespace MusicPlayer.MusicList
+{
+    class SampleMusic : IMusic
+    {
+        private string name   = "Sample";
+        private string artist = "Sota";
+
+        public string MusicName
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string Artist
+        {
+            get => artist;
+            set => artist = value;
+        }
+
+        public void Play()
+        {
+            IElementGetter nameDisplayGetter = MainPage.getInstance();
+            TextBlock nameDisplay            = nameDisplayGetter.textBlock;
+            // ※補足 Environment.NewLine => 改行
+            nameDisplay.Text                 = "The music name is : " + MusicName + Environment.NewLine + 
+                                               "By " + Artist;
+        }
+    }
+}
