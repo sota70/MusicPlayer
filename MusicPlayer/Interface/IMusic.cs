0e040248d0913ab58109c9f16626f5c3f23c21de
sota70 <sota52350@gmail.com>
2021-02-22 13:25:01 +0900

    changed some  codes

--- MusicPlayer/Interface/IMusic.cs
+++ MusicPlayer/Interface/IMusic.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MusicPlayer
 {
-    public interface IMusic : IMusicNameGetter, IMusicNameSetter, IMusicArtistGetter, IMusicArtistSetter
+    public interface IMusic : IMusicNameGetter, IMusicNameSetter, IMusicArtistGetter, IMusicArtistSetter, IMusicDescriptionGetter, IMusicDescriptionSetter
     {
         void Play();
     }
--- MusicPlayer/MusicList/SampleMusic.cs
+++ MusicPlayer/MusicList/SampleMusic.cs
@@ -10,8 +10,9 @@ namespace MusicPlayer.MusicList
 {
     class SampleMusic : IMusic
     {
-        private string name   = "Sample";
-        private string artist = "Sota";
+        private string name        = "Sample";
+        private string artist      = "Sota";
+        private string description = "これはサンプルの曲です";
 
         public string MusicName
         {
@@ -25,13 +26,29 @@ namespace MusicPlayer.MusicList
             set => artist = value;
         }
 
+        public string Description
+        {
+            get => description;
+            set
+            {
+                // ※補足 もし説明文が無かったら”説明なし”と置き換える
+                description            = value;
+                bool descriptionExists = description == null || description.Equals("");
+                if (descriptionExists)
+                {
+                    description = "説明なし";
+                }
+            }
+        }
+
         public void Play()
         {
             IElementGetter nameDisplayGetter = MainPage.getInstance();
             TextBlock nameDisplay            = (TextBlock)nameDisplayGetter.Element;
             // ※補足 Environment.NewLine => 改行
-            nameDisplay.Text                 = "The music name is : " + MusicName + Environment.NewLine + 
-                                               "By " + Artist;
+            nameDisplay.Text                 = "Music Name : " + MusicName + Environment.NewLine +
+                                               "Artist : " + Artist + Environment.NewLine +
+                                               "Description : " + Description; 
         }
     }
 }
